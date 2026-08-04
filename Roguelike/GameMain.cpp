a3f22abd37adee45d9c7c7aa6e3e163f7d5ca86a
Nikita Chervyakov <nikita.chervyakov@1win.pro>
2026-08-04 12:31:37 +0300

    Initial commit

---
+++ Roguelike/GameMain.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+const std::string RESOURCES_PATH = "Resources/";
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Roguelike by HoneyQumo");
+	
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
+}

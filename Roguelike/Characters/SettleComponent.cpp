b83e9c6fad2ff932a3f9f12e725b6ef4a2816d12
Nikita Chervyakov <HoneyQumo@gmail.com>
2026-09-15 10:59:24 +0300

    Feat: труп успокаивается после анимации смерти (#139)
    Доиграв смерть, враг выключает все свои компоненты и компоненты детей, кроме
    тех, что рисуют. Кадр больше не тратится, труп остаётся лежать на месте.

---
+++ Roguelike/Characters/SettleComponent.cpp
@@ -0,0 +1,75 @@
+#include "SettleComponent.h"
+#include <GameObject.h>
+#include <LoggerRegistry.h>
+#include <RectangleRendererComponent.h>
+#include <SpriteRendererComponent.h>
+#include <TransformComponent.h>
+
+namespace RoguelikeGame
+{
+    namespace
+    {
+        bool KeepsDrawing(XYZEngine::Component* component)
+        {
+            return dynamic_cast<XYZEngine::SpriteRendererComponent*>(component) != nullptr
+                || dynamic_cast<XYZEngine::RectangleRendererComponent*>(component) != nullptr
+                || dynamic_cast<XYZEngine::TransformComponent*>(component) != nullptr;
+        }
+    }
+
+    SettleComponent::SettleComponent(XYZEngine::GameObject* gameObject) : Component(gameObject) {}
+
+    void SettleComponent::Start()
+    {
+    }
+
+    void SettleComponent::Update(float deltaTime)
+    {
+        if (isSettled || (isReady != nullptr && !isReady()))
+        {
+            return;
+        }
+
+        Settle();
+    }
+
+    void SettleComponent::Render()
+    {
+    }
+
+    void SettleComponent::SetReadyCheck(std::function<bool()> newIsReady)
+    {
+        isReady = std::move(newIsReady);
+    }
+
+    bool SettleComponent::IsSettled() const
+    {
+        return isSettled;
+    }
+
+    void SettleComponent::Settle()
+    {
+        if (isSettled)
+        {
+            return;
+        }
+
+        isSettled = true;
+
+        int quieted = 0;
+        for (XYZEngine::Component* component : gameObject->GetComponentsInChildren<XYZEngine::Component>())
+        {
+            if (component == this || component == nullptr || KeepsDrawing(component) || !component->IsEnabled())
+            {
+                continue;
+            }
+
+            component->SetEnabled(false);
+            quieted++;
+        }
+
+        SetEnabled(false);
+
+        LOG_INFO(gameObject->GetName() + " settles down, " + std::to_string(quieted) + " components are quiet");
+    }
+}
